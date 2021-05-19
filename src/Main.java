e329e1e255087dde5e26c5841e0a32d45161c7bc
MarijeSchut <marije1217@gmail.com>
2021-05-19 14:19:04 +0200

    Hotelkamer

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         bed hiltonBed = new bed(2.5,2.1,4,2);
         seize hiltonSeize= new seize(6,5,3);
 
-        hotelroom hiltonRoom = new hotelroom(hiltonSeize,hiltonBed,true);
+        hotelroom hiltonRoom = new hotelroom(hiltonSeize,hiltonBed,true, "Zee");
 
         System.out.println(hiltonRoom.getBed().getKussen());
 
